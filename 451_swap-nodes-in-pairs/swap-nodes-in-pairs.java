55edfcd3cfb535949f31a5a7db8e931ad7bd958f
Conan1985 <youwei_lu@hotmail.com>
2017-05-17 07:12:53 -0400

    Fix swap nodes in pairs.

---
+++ 451_swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,38 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/swap-nodes-in-pairs
+@Language: Java
+@Datetime: 17-05-16 21:08
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head a ListNode
+     * @return a ListNode
+     */
+    public ListNode swapPairs(ListNode head) {
+        // write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null && head.next.next != null) {
+            ListNode n1 = head.next;
+            ListNode n2 = head.next.next;
+            n1.next = n2.next;
+            n2.next = n1;
+            head.next = n2;
+            head = n1;
+        }
+        return dummy.next;
+    }
+}
