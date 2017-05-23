20f9efc7e689ee78294a68786383a2980e327115
Conan1985 <youwei_lu@hotmail.com>
2017-05-23 06:20:04 -0400

    Add more files in Chapter 5

---
+++ 494_implement-stack-by-two-queues/implement-stack-by-two-queues.java
@@ -0,0 +1,60 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/implement-stack-by-two-queues
+@Language: Java
+@Datetime: 17-05-23 01:09
+*/
+
+class Stack {
+    // Push a new item into the stack
+    
+    LinkedList queue1 = new LinkedList();
+    LinkedList queue2 = new LinkedList();
+    
+    public void push(int x) {
+        // Write your code here
+        queue1.add(x);
+    }
+
+    // Pop the top of the stack
+    public void pop() {
+        // Write your code here
+        while (queue1.size()>1) {
+            queue2.add(queue1.getFirst());
+            queue1.removeFirst();
+        }
+        queue1.removeFirst();
+        while (queue2.size()>0) {
+            queue1.add(queue2.getFirst());
+            queue2.removeFirst();
+        }
+    }
+
+    // Return the top of the stack
+    public int top() {
+        // Write your code here
+        while (queue1.size()>1) {
+            queue2.add(queue1.getFirst());
+            queue1.removeFirst();
+        }
+        int result = (int) queue1.getFirst();
+        queue1.removeFirst();
+        queue2.add(result);
+        while (queue2.size()>0) {
+            queue1.add(queue2.getFirst());
+            queue2.removeFirst();
+        }
+        return result;
+    }
+
+    // Check the stack is empty or not.
+    public boolean isEmpty() {
+        // Write your code here
+        if (queue1.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ 495_implement-stack/implement-stack.java
@@ -0,0 +1,40 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/implement-stack
+@Language: Java
+@Datetime: 17-05-22 20:33
+*/
+
+class Stack {
+    private LinkedList ls = new LinkedList();
+    
+    // Push a new item into the stack
+    public void push(int x) {
+        // Write your code here
+        ls.add(x);
+    }
+
+    // Pop the top of the stack
+    public void pop() {
+        // Write your code here
+        ls.removeLast();
+    }
+
+    // Return the top of the stack
+    public int top() {
+        // Write your code here
+        return (int)ls.getLast();
+        
+    }
+
+    // Check the stack is empty or not.
+    public boolean isEmpty() {
+        // Write your code here
+        if (ls.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }    
+}
