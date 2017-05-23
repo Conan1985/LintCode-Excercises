591f54f0055a788e6a57c99a275bb64ca0ade636
Conan1985 <youwei_lu@hotmail.com>
2017-05-23 07:30:00 -0400

    Add queue by linked-list problems.

---
+++ 492_implement-queue-by-linked-list/implement-queue-by-linked-list.java
@@ -0,0 +1,28 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/implement-queue-by-linked-list
+@Language: Java
+@Datetime: 17-05-23 11:25
+*/
+
+public class MyQueue {
+    private LinkedList ls;
+    
+    public MyQueue() {
+        // do initialize if necessary
+        ls = new LinkedList();
+    }
+
+    public void enqueue(int item) {
+        // Write your code here
+        ls.addLast(item);
+    }
+
+    public int dequeue() {
+        // Write your code here
+        int result = (int)ls.getFirst();
+        ls.removeFirst();
+        return result;
+    }
+}
---
+++ 493_implement-queue-by-linked-list-ii/implement-queue-by-linked-list-ii.java
@@ -0,0 +1,40 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/implement-queue-by-linked-list-ii
+@Language: Java
+@Datetime: 17-05-23 11:23
+*/
+
+public class Dequeue {
+    private LinkedList ls;
+    
+    public Dequeue() {
+        // do initialize if necessary
+        ls = new LinkedList();
+    }
+
+    public void push_front(int item) {
+        // Write your code here
+        ls.addFirst(item);
+    }
+
+    public void push_back(int item) {
+        // Write your code here
+        ls.addLast(item);
+    }
+
+    public int pop_front() {
+        // Write your code here
+        int result = (int)ls.getFirst();
+        ls.removeFirst();
+        return result;
+    }
+
+    public int pop_back() {
+        // Write your code here
+        int result = (int)ls.getLast();
+        ls.removeLast();
+        return result;
+    }
+}
