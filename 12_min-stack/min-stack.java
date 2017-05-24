35d352d4e088921e4a0f3124ae5809acf6c0ff05
Conan1985 <youwei_lu@hotmail.com>
2017-05-23 20:38:49 -0400

    Complete optional problems of Chapter 5.

---
+++ 12_min-stack/min-stack.java
@@ -0,0 +1,39 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/min-stack
+@Language: Java
+@Datetime: 17-05-23 15:58
+*/
+
+public class MinStack {
+    Stack st;
+    Stack sm;
+    
+    public MinStack() {
+        // do initialize if necessary
+        st = new Stack();
+        sm = new Stack();
+    }
+
+    public void push(int number) {
+        // write your code here
+        if (st.empty() || (int)sm.peek() > number) {
+            sm.push(number);
+        } else {
+            sm.push(sm.peek());
+        }
+        st.push(number);
+    }
+
+    public int pop() {
+        // write your code here
+        sm.pop();
+        return (int)st.pop();
+    }
+
+    public int min() {
+        // write your code here
+        return (int)sm.peek();
+    }
+}
