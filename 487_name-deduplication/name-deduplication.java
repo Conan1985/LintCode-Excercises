b5e0243de997648fdd3b4306c5e001c1fa837d8d
Conan1985 <youwei_lu@hotmail.com>
2017-06-06 18:27:04 -0400

    Add three problems in Chapter 7.

---
+++ 487_name-deduplication/name-deduplication.java
@@ -0,0 +1,27 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/name-deduplication
+@Language: Java
+@Datetime: 17-06-06 15:41
+*/
+
+public class Solution {
+    /**
+     * @param names a string array
+     * @return a string array
+     */
+    public List<String> nameDeduplication(String[] names) {
+        // Write your code here
+        List<String> result = new ArrayList<>();
+        HashSet<String> hs = new HashSet<>();
+        for (int i = 0; i < names.length; i++) {
+            String str = names[i].toLowerCase();
+            if (!hs.contains(str)) {
+                hs.add(str);
+                result.add(str);
+            }
+        }
+        return result;
+    }
+}
