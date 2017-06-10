068091fc979127e32d4241c20509a9f01d689c49
Conan1985 <youwei_lu@hotmail.com>
2017-06-10 12:05:47 -0400

    Add files for Chapter 7 of Basic Java.

---
+++ 124_longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,36 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/longest-consecutive-sequence
+@Language: Java
+@Datetime: 17-06-10 13:13
+*/
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return an integer
+     */
+    public int longestConsecutive(int[] nums) {
+        // write your code here
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        int longest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int down = nums[i] - 1;
+            while (set.contains(down)) {
+                set.remove(down);
+                down--;
+            }
+            int up = nums[i] + 1;
+            while (set.contains(up)) {
+                set.remove(up);
+                up++;
+            }
+            longest = Math.max(longest, up - down - 1);
+        }
+        return longest;
+    }
+}
---
+++ 138_subarray-sum/subarray-sum.java
@@ -0,0 +1,41 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/subarray-sum
+@Language: Java
+@Datetime: 17-06-10 12:31
+*/
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        // write your code here
+        int sum = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        for (int i = 0; i < nums.length; i ++) {
+            sum = 0;
+            sum += nums[i];
+            if (sum == 0) {
+                result.add(i);
+                result.add(i);
+                return result;
+            }
+            for (int j = i + 1; j < nums.length; j ++) {
+                sum += nums[j];
+                if (sum == 0){
+                    result.add(i);
+                    result.add(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
