915a3d9046c8c2bbed9440d62c49b3d968225c3e
Conan1985 <youwei_lu@hotmail.com>
2017-06-13 07:42:55 -0400

    Add sort integers.

---
+++ 463_sort-integers/sort-integers.java
@@ -0,0 +1,29 @@
+/*
+@Copyright:LintCode
+@Author:   Conan_1985
+@Problem:  http://www.lintcode.com/problem/sort-integers
+@Language: Java
+@Datetime: 17-06-13 11:41
+*/
+
+public class Solution {
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    public void sortIntegers(int[] A) {
+        // Write your code here
+        if (A.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < A.length-1; i++) {
+            for (int j = i+1; j < A.length; j++) {
+                if (A[j] < A[i]) {
+                    int temp = A[i];
+                    A[i] = A[j];
+                    A[j] = temp;
+                }
+            }
+        }
+    }
+}
